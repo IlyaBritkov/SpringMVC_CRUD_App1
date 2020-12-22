569a7305ca7ee44fedb22aa5fc821a7c9bbe6ff1
Ilya <ilya500514@gmail.com>
2020-12-22 15:24:31 +0300

    Added Update person operation into PeopleController.java and PersonDAO.java via update() methods. Created edit.html for display of edit page. Added hiddenHttpMethodFilter into MySpringMvcDispatcherServletInitializer.java. A little changes in other files.

--- src/main/java/com/example/springcourse/controllers/PeopleController.java
+++ src/main/java/com/example/springcourse/controllers/PeopleController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 @RequestMapping("/people")
 public class PeopleController {
+
     private final PersonDAO personDAO;
 
     @Autowired
@@ -19,14 +20,12 @@ public class PeopleController {
 
     @GetMapping()
     public String index(Model model) {
-        // receive all people from DAO
         model.addAttribute("people", personDAO.index());
         return "people/index";
     }
 
     @GetMapping("/{id}")
     public String show(@PathVariable("id") int id, Model model) {
-        // receive person by id from DAO
         model.addAttribute("person", personDAO.show(id));
         return "people/show";
     }
@@ -41,4 +40,18 @@ public class PeopleController {
         personDAO.save(person);
         return "redirect:/people";
     }
+
+    @GetMapping("/{id}/edit")
+    public String edit(Model model, @PathVariable("id") int id) {
+        model.addAttribute("person", personDAO.show(id));
+        return "people/edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("person") Person person, @PathVariable("id") int id) {
+        personDAO.update(id, person);
+        return "redirect:/people";
+    }
+
+
 }
